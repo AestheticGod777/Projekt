3f64c511d60ffec72aa4d708ff2907cd28dd698e
Roland1520 <troznairoland@gmail.com>
2021-03-26 14:27:27 +0100

    nyolc

--- trapez.py
+++ trapez.py
@@ -22,21 +22,34 @@ class Kattintgato:
         self.turtle.forward(100)
         self.turtle.pendown()
 
-    def nulla(self):
+    def nyolc(self):
+        for i in range(2):
+            self.turtle.left(90)
+            self.trapez()
+            self.trapez()
+            self.turtle.left(90)
+            self.trapez()
+        self.turtle.penup()
         self.turtle.left(90)
-        self.trapez()
-        self.trapez()
-        self.turtle.left(90)
-        self.trapez()
-        self.turtle.left(90)
-        self.trapez()
-        self.trapez()
-        self.turtle.left(90)
-        self.trapez()
+        self.turtle.forward(98)
+        self.turtle.pendown()
+        self.turtle.left(135)
+        self.turtle.forward(20)
+        self.turtle.right(45)
+        self.turtle.forward(70)
+        self.turtle.right(45)
+        self.turtle.forward(20)
+        self.turtle.right(90)
+        self.turtle.forward(20)
+        self.turtle.right(45)
+        self.turtle.forward(70)
+        self.turtle.right(45)
+        self.turtle.forward(20)
+        self.turtle.right(45)
 
     def __init__(self):
         self.turtle.speed(0)
-        self.nulla()
+        self.nyolc()
 
         self.screen.mainloop()
 
